71e8fd18d04ce9a3c821f899205540c2f84fef5f
Zodi-Star <zodistar@protonmail.com>
2026-02-23 18:53:08 -0500

    Update workspaces.ts

--- db/workspaces.ts
+++ db/workspaces.ts
@@ -1,26 +1,29 @@
-export const getHomeWorkspaceByUserId = async () => {
+export const getHomeWorkspaceByUserId = async (_userId?: string) => {
   return {
     id: "default",
     name: "Default Workspace"
   }
 }
 
-export const getWorkspaceById = async () => {
-  return null
+export const getWorkspaceById = async (_workspaceId?: string) => {
+  return {
+    id: "default",
+    name: "Default Workspace"
+  }
 }
 
-export const getWorkspacesByUserId = async () => {
+export const getWorkspacesByUserId = async (_userId?: string) => {
   return []
 }
 
-export const createWorkspace = async () => {
+export const createWorkspace = async (_data?: any) => {
   return null
 }
 
-export const updateWorkspace = async () => {
+export const updateWorkspace = async (_workspaceId?: string, _data?: any) => {
   return null
 }
 
-export const deleteWorkspace = async () => {
+export const deleteWorkspace = async (_workspaceId?: string) => {
   return null
 }
